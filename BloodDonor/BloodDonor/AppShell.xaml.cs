163accb125d3f5f9a254c6afeedcdfcf525e9be7
Emil <ahmetovic.emil@fsk.unsa.ba>
2025-04-14 21:40:36 +0200

    21:40 Commit

--- BloodDonor/BloodDonor/AppShell.xaml.cs
+++ BloodDonor/BloodDonor/AppShell.xaml.cs
@@ -1,10 +1,9 @@
-﻿namespace BloodDonor
+﻿namespace BloodDonor;
+
+public partial class AppShell : Shell
 {
-    public partial class AppShell : Shell
+    public AppShell()
     {
-        public AppShell()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
     }
 }
