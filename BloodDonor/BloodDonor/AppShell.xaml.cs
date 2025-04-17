bff9da7c1d32297ee7e2ebf2e03abf37d9c86f14
Emil <ahmetovic.emil@fsk.unsa.ba>
2025-04-17 12:58:05 +0200

    Welcome page, LoginPage,RegisterPage 17.4.2025 / 12:57

--- BloodDonor/BloodDonor/AppShell.xaml.cs
+++ BloodDonor/BloodDonor/AppShell.xaml.cs
@@ -1,9 +1,16 @@
-﻿namespace BloodDonor;
+﻿using BloodDonor.Views;
+
+namespace BloodDonor;
 
 public partial class AppShell : Shell
 {
     public AppShell()
     {
         InitializeComponent();
+
+        // Register routes for navigation
+        Routing.RegisterRoute("WelcomePage", typeof(WelcomePage));
+        Routing.RegisterRoute("LoginPage", typeof(LoginPage));
+        Routing.RegisterRoute("RegisterPage", typeof(RegisterPage));
     }
 }
