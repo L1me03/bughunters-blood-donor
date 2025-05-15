4575edd7e1db17b251c2ae425936cfb98e884de1
tarikcorbo <ceotare@gmail.com>
2025-05-15 16:31:57 +0200

    Ubacen kod, ali nesto ne radi fino

--- BloodDonor/AplikacijaDonorApp2/MapPage.xaml.cs
+++ BloodDonor/AplikacijaDonorApp2/MapPage.xaml.cs
@@ -1,3 +1,7 @@
+using System.Net.NetworkInformation;
+using Microsoft.Maui.Controls.Maps;
+using Microsoft.Maui.Maps;
+
 namespace AplikacijaDonorApp2.Views;
 
 public partial class MapPage : ContentPage
@@ -5,6 +9,20 @@ public partial class MapPage : ContentPage
     public MapPage()
     {
         InitializeComponent();
+
+        // Centriraj mapu na Sarajevo
+        donorMap.MoveToRegion(MapSpan.FromCenterAndRadius(
+            new Location(43.8563, 18.4131), Distance.FromKilometers(5)));
+
+        // Dodaj jedan pin za test
+        var testPin = new Pin
+        {
+            Label = "Donor: Marko",
+            Address = "Kranjèeviæeva, Sarajevo",
+            Location = new Location(43.8563, 18.4131),
+            Type = PinType.Place
+        };
+
+        donorMap.Pins.Add(testPin);
     }
 }
-
